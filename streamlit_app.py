aa0a98b62a801110c88e0282d8521adbb28d88ca
KFM06 <118633243+KFM06@users.noreply.github.com>
2023-07-31 14:39:00 -0230

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
-import folium
+import streamlit as st
+st.sidebar.<widget>
+a = st.sidebar.radio('R:',[1,2])
 
-#Creating the map object
-m = folium.Map(location=[49.1852, -57.4184], zoom_start=12)
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.caption('Balloons. Hundreds of them...')
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
 
-#Generating the map
-m.save('map.html')
+* optional kwarg unsafe_allow_html = True
